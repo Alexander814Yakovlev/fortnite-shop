89ca476e8c7a84d8fe8ba7bd261f5c15e53cb29d
Alex <cahek814@gmail.com>
2024-05-09 12:51:26 +0300

    env fix

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
-// const API_KEY = process.env.REACT_APP_API_KEY
-const API_KEY = '3eadbe46-8d33b3f7-0d802714-6069d277'
+const API_KEY = process.env.REACT_APP_API_KEY
 const API_URL = 'https://fortniteapi.io/v2/shop?lang=ru'
 
 export { API_KEY, API_URL }
